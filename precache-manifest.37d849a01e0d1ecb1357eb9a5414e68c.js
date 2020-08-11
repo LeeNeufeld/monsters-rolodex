513d2dc335b58ccf9d7ec688b3630a4f2a13a8d2
LeeNe <leeneufeld23@hotmail.com>
2020-08-11 14:22:36 -0600

    Updates

---
+++ precache-manifest.37d849a01e0d1ecb1357eb9a5414e68c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af92966c1f457b649a514b926df0041",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a057b3b1698d84962e00",
+    "url": "/monsters-rolodex/static/css/main.712f6c08.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a057b3b1698d84962e00",
+    "url": "/monsters-rolodex/static/js/main.4666626a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
